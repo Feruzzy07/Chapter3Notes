daffa7685551de57f5ee55e2c35b84e3be90637f
526o <526o@fresnou.org>
2023-03-24 14:18:35 -0700

    started array notes + exercises was absent yesterday

---
+++ arrays1.js
@@ -0,0 +1,36 @@
+// //Question 1
+// function firstElement(array){
+//     console.log(array[0]);
+// }
+// firstElement([1,2,3,4,5]);
+
+//Question 2
+// function firstElement(array){
+//     console.log(array[4]);
+// }
+// firstElement([1,2,3,4,5]);
+
+//Question 3
+// function firstElement(array){
+//     if(array.length == 0){
+//         console.log("empty array");
+//     }
+//     else{
+//         console.log(array[0]);
+//     }
+//  }
+//  firstElement([]);
+
+//Question 4
+// function element(array){
+//     for(let i = array.length - 1; i >= 0; i--){
+//         console.log(array[i]);
+//     }
+// }
+// element([5,4,3,2,1]);
+
+//Question 5
+function element(array){
+for()
+}
+
