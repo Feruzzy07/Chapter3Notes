bdb45f8fccf9d3e641dc8bf3020bf9565abbd43d
526o <526o@fresnou.org>
2023-03-28 14:18:29 -0700

    did arrays exercises

--- arrays2.js
+++ arrays2.js
@@ -34,4 +34,59 @@
 // replace([1,2,3,4,5], 5);
 
 //Question 4
-function change
+// function change(arr, x){
+//     let newarr = [];
+//     for(let i = 0; i < arr.length; i++){
+//         newarr.push(arr[i]);
+//         newarr[i]= x;
+//     }
+//     console.log(arr);
+//     console.log(newarr);
+// }
+// change([1,2,3,4,5], 5);
+
+//Question 5
+// function compare(arr){
+//     let first = arr[0];
+//     let last = arr.length - 0;
+//     let newarr = [];
+//     if(first > last){
+//         for(let i = 0; i < arr.length; i++){
+//             newarr.push(arr[i]);
+//             newarr[i]= first;
+//         }
+//         console.log(newarr);
+//     }else{
+//         for(let i = 0; i < arr.length; i++){
+//             newarr.push(arr[i]);
+//             newarr[i]= last;
+//         }
+//         console.log(newarr);
+//     }
+// }
+// compare([6,2,3,4,5]);
+
+//Question 6
+// function compare(arr){
+//     let first = arr[0];
+//     let last = arr.length - 0;
+//     let newarr = [];
+//     if(first > last){
+//         for(let i = 0; i < arr.length; i++){
+//             newarr.push(arr[i]);
+//             newarr[i]= first;
+//         }
+//         console.log(arr);
+//         console.log(newarr);
+//     }else{
+//         for(let i = 0; i < arr.length; i++){
+//             newarr.push(arr[i]);
+//             newarr[i]= last;
+//         }
+//         console.log(arr);
+//         console.log(newarr);
+//     }
+// }
+// compare([6,2,3,4,5]);
+
+//Question 7
