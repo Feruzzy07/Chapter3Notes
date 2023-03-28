045765d326d130d5ecc781fb45e6e869107745f1
526o <526o@fresnou.org>
2023-03-28 14:02:01 -0700

    did arrays exercises

--- arrays2.js
+++ arrays2.js
@@ -1,9 +1,37 @@
 //Question 1
-function population(n){
-    let pop = [];
-    for (let i = 1; i <= n; i++){
-        pop.push(i);
-    }
-    console.log(pop);
-}
-population(5);
+// function population(n){
+//     let pop = [];
+//     for (let i = 1; i <= n; i++){
+//         pop.push(i)(
+//     }
+//     console.log(pop);
+// }
+// population(5);
+
+//Question 2
+// function population(n){
+//     let pop = [];
+//     if(n <= 0){
+//         console.log('invalid');
+//     }else {
+//         for (let i = 1; i <= n; i++) {
+//             pop.push(i);
+//         }
+//         console.log(pop);
+//     }
+// }
+// population(0);
+
+//Question 3
+// function replace(arr, x){
+//     let newarr = [];
+//     for(let i = 0; i < arr.length; i++){
+//         newarr.push(arr[i]);
+//         newarr[i]= x;
+//     }
+//     console.log(newarr);
+// }
+// replace([1,2,3,4,5], 5);
+
+//Question 4
+function change
