61fc3bf0a1ad6233064310f5dd9b38859f6ff91b
526o <526o@fresnou.org>
2023-04-14 14:18:20 -0700

    magic 8 ball extension

--- magic8.js
+++ magic8.js
@@ -5,6 +5,20 @@ let answers=['yes', 'no', 'maybe', 'theoratically yes', 'Whatever you wanted the
 let randomNum = Math.floor(Math.random()*answers.length);
 let answer = answers[randomNum];
 
+
+let input = readLine.question('I am the all mighty magic 8 ball. Before we begin, do you have any additional outputs you would add to our program? Enter y or n: ');
+if(input === 'y'){
+        let awnser = readLine.question('What potential outputs would you want to add?: ');
+        answers.push(awnser);
+        let again = readLine.question('Do you have anymore outputs you would want to add?: y or n: ');
+        again = again.toLowerCase();
+}else if(input === 'n'){
+        console.log('I am the all mighty magic 8 ball. Give me your yes or no question! NOW!');
+        readLine.question('What is your question?: ');
+        console.log(answer);
+}else{
+    console.log('Invalid Answer, Try again next time!');
+}
 console.log('I am the all mighty magic 8 ball. Give me your yes or no question! NOW!');
 readLine.question('What is your question?: ');
 console.log(answer);
