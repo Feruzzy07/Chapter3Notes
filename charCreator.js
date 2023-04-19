6ca381ab535d0f75fa4696fbc807e20e530fe13d
526o <526o@fresnou.org>
2023-04-19 14:18:32 -0700

    char creator

--- charCreator.js
+++ charCreator.js
@@ -1,11 +1,23 @@
 const readLine = require('readline-sync');
 
 let diceRolled = [];
+let eyeColors = ['blue', 'green', 'brown', 'hazel'];
+let Class = ['Barbarian', 'Hunter', 'Healer', 'Nerd', 'Peasent', 'King', 'Wizard'];
+let specialSkill = ['Singing', 'Killing', 'Torturing', 'Dancing', 'Archery', 'Horse Rider', 'Eating'];
+let money = [12, 1094, 94097832, 93892, 4942, 192, 0, 92, 5923];
 let character1 = {
     //empty for now
 };
+let character2 = {
+
+};
+let character3 = {
+
+};
 
 createChar(character1, diceRolled);
+createChar(character2, diceRolled);
+createChar(character3, diceRolled);
 function rolld6(){
     return Math.ceil(Math.random()*6);
 }
@@ -13,7 +25,7 @@ function rolld6(){
 function rollDice(amount, list) {
     for (let i = 0; i < amount; i++) {
         let roll = rolld6();
-        console.log(`You rolled a ${roll}!`);
+        // console.log(`You rolled a ${roll}!`);
         list.push(roll);
     }
 }
@@ -23,7 +35,7 @@ function sum(list){
     for(let i = 0; i < list.length; i++){
         sum+=list[i]
     }
-    console.log(`The total was: ${sum}.`);
+    // console.log(`The total was: ${sum}.`);
     return sum;
 }
 
@@ -33,31 +45,46 @@ function rollStat(list){
     return sum(list);
 }
 
+function pickRandomChoice(choices){
+    let randomNum = Math.floor(Math.random()*choices.length);
+    return choices[randomNum];
+}
+
 function createChar(character, list){
     character.name = readLine.question("Enter Character Name: ");
-    console.log("Rolling Strength");
+    character.eyeColor = pickRandomChoice(eyeColors);
+    // console.log("Rolling Strength");
     character.strength = rollStat(diceRolled);
-    console.log("Rolling Dexterity");
+    // console.log("Rolling Dexterity");
     character.dexterity = rollStat(diceRolled);
-    console.log("Rolling Constitution");
+    // console.log("Rolling Constitution");
     character.constitution = rollStat(diceRolled);
-    console.log("Rolling Intelligence");
+    // console.log("Rolling Intelligence");
     character.intelligence = rollStat(diceRolled);
-    console.log("Rolling Wisdom");
+    // console.log("Rolling Wisdom");
     character.wisdom = rollStat(diceRolled);
-    console.log("Rolling Charisma");
+    // console.log("Rolling Charisma");
     character.charisma = rollStat(diceRolled);
+    character.luck = rolld6();
+    character.class = pickRandomChoice(Class);
+    character.specialskill = pickRandomChoice(specialSkill);
+    character.money = pickRandomChoice(money);
     displayChar(character);
 }
 
 function displayChar(char){
     console.log(`----------------------------`);
     console.log(`Name: ${char.name}`);
+    console.log(`Eye Color: ${char.eyeColor}`);
     console.log(`STR: ${char.strength}`);
     console.log(`DEX: ${char.dexterity}`);
     console.log(`CON: ${char.constitution}`);
     console.log(`INT: ${char.intelligence}`);
     console.log(`WIS: ${char.wisdom}`);
     console.log(`CHAR: ${char.charisma}`);
+    console.log(`LUCK: ${char.luck}`);
+    console.log(`Class: ${char.class}`);
+    console.log(`Special Skill: ${char.specialskill}`);
+    console.log(`Money: ${char.money}`);
     console.log(`----------------------------`);
 }
